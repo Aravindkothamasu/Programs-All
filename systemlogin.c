418f1d8fbaddcb99629fbe029b332f506fc93ea0
Aravind Kothamasu <51799397+Aravindkothamasu@users.noreply.github.com>
2020-02-05 17:44:08 +0530

    Add files via upload

--- systemlogin.c
+++ systemlogin.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<strings.h>
+#include<string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -16,21 +17,48 @@ void main(int argc,char **argv)
 		return;
 	}
  
-	char str [30] = "ping 192.168.1.";
+	char str [40],buf[400];
 	int i,j,fd=0;
 
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
+	j = atoi(argv[1]);
+	i = atoi(argv[2]);
+
+	
 //	for(  ; i<256 ; i++)
-		for(j = atoi(argv[2]) ; j < 256 ;j++)
+		for(  ; j <= i  ; j++ )
 		{
 			if(!fork())
 			{
+				
+				int temp = open("Temp",O_CREAT | O_RDONLY | O_WRONLY | O_TRUNC ,0664);
+				int fd = open("vurikey",O_APPEND | O_RDONLY | O_WRONLY | O_CREAT,0664);
+				if(temp < 0 || fd < 0)
+				{
+					perror("error in opening any of the file.....\n");
+					return;
+				}
+
 				close(1);
-				fd = open("vurikey",O_APPEND | O_RDWR | O_CREAT,0664);
-				sprintf(str,"ping 192.168.1.%d",j);
+				sprintf(str,"ping -c 3 192.168.1.%d",j);
 				system(str);
+				read(temp,buf,sizeof(buf));
+				
+				if(strcmp(buf,"ttl=64") == 0)				
+				{
+					sprintf(buf,"192.168.1.%d             Ubuntu\n",j);
+					write(fd,buf,strlen(buf));
+				}				
+				else if(strcmp(buf,"ttl=128") == 0)
+				{
+					sprintf(buf,"192.168.1.%d             Windows\n",j);
+					write(fd,buf,strlen(buf));
+				}
+				close(temp);
+				close(fd);
 			}
+			else
+				exit(0);
+
 		}
 }
 
