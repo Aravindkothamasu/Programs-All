5696e91cef0fe2e5d76be1dc7ec10796982f6a41
Aravind Kothamasu <51799397+Aravindkothamasu@users.noreply.github.com>
2020-06-10 16:25:05 +0530

    Update rb_header.h

--- Algorithm/Red Black Tree/rb_header.h
+++ Algorithm/Red Black Tree/rb_header.h
@@ -1,12 +1,18 @@
-
+////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef __RB_HEADER_H__
 #define __RB_HEADER_H__
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<strings.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
 typedef struct base
 {
 	struct base *parent;
@@ -15,6 +21,7 @@ typedef struct base
 	int flag;					//RED or  BLACK
 	int num;
 }B;
+
 char input(int *); 
 void Parsing(int ,char **,B **);
 int height(B *);
@@ -22,12 +29,27 @@ int height(B *);
 int * filter (B *,int,int );
 void create(B **,int);
 int check(B*);
+B* AllRed(B *,B**);
 
-int search(B **,int );
+
+
+int element(char *);
+char NextEnti(B **);
+void my_random(B **,int );
+void File(B **,char *);
+void helpMsg(void);
+void string(B **,char *);
+void FilePrint(B *,FILE *);
+void FileAddr(B **,FILE *);
+void CopytoFile(B **);
+B * Violations(B *,B **,int);
+int search(B *,int );
 void deletion(B **,int );
+int * Addr_search(B **,int );
 
 
-void rotation(B **,B **);
+
+B* rotation(B *,B **);
 void Rrotate(B *,B**);
 void Lrotate(B *,B**);
 
@@ -39,4 +61,7 @@ void PrintAddr(B **);
 
 void Flag_print(B *);
 void print(B *);
+void Author(void);
 #endif
+
+////////////////////////////////////////////////////////////////////////////////////
