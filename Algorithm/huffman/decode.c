1f4422afe082c28a3f0fdbc11bfd67a123878982
Aravind Kothamasu <51799397+Aravindkothamasu@users.noreply.github.com>
2021-03-21 14:53:34 +0530

    Just type chesanu enka check cheyalla

--- Algorithm/huffman/decode.c
+++ Algorithm/huffman/decode.c
@@ -1,20 +1,87 @@
 #include "Huffman_Header.h"
 
 void main (int argc, char **argv)
-{
+{  
+	char *Ext;
+	int   FileDes = -1;
+	
 	CmdLineCheck(argc);
+	CheckFile( argv[0]);
+	InFileDes = OpenInFile( argv[0] );
+	Ext = GetOutFileExtent ( InFileDes, argv[0] );
+	OutFileName = CreateOutFileName( Ext, argv[0]);
 	
-	
+	FileDes = OpenOutFile( OutFileName);
+	if (FileDes < 0 )
+	{
+		console_print("Error in Creating FileName : %s\n", OutFileName);
+		return;
+	}
 	
 	
 	
 }
 
-int CmdLineCheck(int argc)
+char * GetOutFileExtent( int InFileDes, char *InFileName)
+{
+	char 
+
+	return ;
+}
+
+int OpenInFile( char *FileName)
+{
+	int FileDes = -1;
+	
+	FileDes = open( FileName, READ_MODE_FILE );
+	if( FileDes < 0)
+		console_print("Unable to open the input File : %s Reason : %s\n",
+			FileName, strerror(errno));
+	else
+		console_print("File Open SUCCESS\n");
+		
+	return FileDes;
+}
+
+char *GetOutFileExtent (char *BinFileName)
+{
+	
+	
+	
+}
+
+
+void CheckFile( char *FileName)
+{
+	char temp[5] = ".bin";
+	int len = 0;
+	
+	len = strlen( FileName) -1;
+	
+	for( i=0;i<4;i++)
+		if( temp[i] != FileName[len-3+i] )
+		{
+			console_print("Error in FileName extention : %s\n", FileName);
+			exit(0);
+		}
+}
+
+void CmdLineCheck(int argc)
 {
   if( argc != 2 )
   {   
     console_print("ERROR CMLD LINE USAGE : ./Decode   [Input File.bin]\n");
 	exit(0);
   }
+}
+
+void FramingData( int Line, const char *Func, const char *File, const char *format, ...)
+{								    
+  char Buffer[500]={0};
+  va_list args;
+  va_start( args, format);					    
+  sprintf( Buffer, "#%s#  %s() [%d] : ", File, Func, Line);	    
+  vsprintf( &Buffer[strlen(Buffer)], format, args);		    
+  va_end(args);	
+  printf("%s", Buffer);
 }
