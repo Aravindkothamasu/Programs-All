72ebdd7043103ecbe01011bb907f8ccee274c506
Aravind Kothamasu <aravindkothamasu@gmail.com>
2026-01-16 23:11:04 +0530

    SORTING: Fixed intentation

--- Algorithm/Sorting/sorting_algos.c
+++ Algorithm/Sorting/sorting_algos.c
@@ -6,36 +6,37 @@ FILE *FileDes;
 
 void main(int argc,char **argv)
 {
-  int array[MAX_ENTRIES];
-  Ip_rand(array, MAX_ENTRIES);
+    int array[MAX_ENTRIES];
+    Ip_rand(array, MAX_ENTRIES);
 
-  /*		selectionSort(array,n);
-		console_console_print(array,n);
-		bubbleSort(array,n);
-		console_console_print(array,n);	
-		heapSort(array,n);
-   */
-  console_print(array,MAX_ENTRIES);
-  quickSort(array,MAX_ENTRIES);
-  console_print(array,MAX_ENTRIES);
+    /*
+       selectionSort(array,n);
+       console_console_print(array,n);
+       bubbleSort(array,n);
+       console_console_print(array,n);	
+       heapSort(array,n);
+     */
+    console_print(array,MAX_ENTRIES);
+    quickSort(array,MAX_ENTRIES);
+    console_print(array,MAX_ENTRIES);
 }
 
 
 void Read_file(int *array,int n)
 {
-  int i;
-  for(i=0;i<n;i++)
-    fscanf(FileDes,"%d",&array[i]);
+    int i;
+    for(i=0;i<n;i++)
+	    fscanf(FileDes,"%d",&array[i]);
 }
 
 void Open_file(char *filename)
 {
-  FileDes = fopen(filename,"r+");
-  if(FileDes == 0)
-  {
-    printf("Unable to openfile ---->%s\n",filename);
-    exit(0);
-  }
+    FileDes = fopen(filename,"r+");
+    if(FileDes == 0)
+    {
+	    printf("Unable to openfile ---->%s\n",filename);
+	    exit(0);
+    }
 }
 
 void Ip_rand(int *array,int EntryCount)
@@ -44,91 +45,91 @@ void Ip_rand(int *array,int EntryCount)
     srand(getpid());
     for(i=0; i< EntryCount; i++)
     {
-        array[i] = rand()%1000;
+	    array[i] = rand()%1000;
     }
 }
 
 void console_print(int *array,int n)
 {
-  printf("\n");
-  for(int i=0;i<n;i++)
-    printf("%d\t",array[i]);
-  printf("\n");
+    printf("\n");
+    for(int i=0;i<n;i++)
+	    printf("%d\t",array[i]);
+    printf("\n");
 }
 
 void selectionSort(int *array,int n)
 {
     int i,j;
     for(i=0;i<n-1;i++)
-        for(j=i+1;j<n;j++)
-            if(array[i]>array[j])		//Change should happen
-	            swap(array,i,j);	
+	    for(j=i+1;j<n;j++)
+	        if(array[i]>array[j])		//Change should happen
+		        swap(array,i,j);	
 }
 
 void bubbleSort(int *array,int n)
 {
-  int i,j;
-  for(i=0;i<n;n--,i=0)
-    for(j=i+1;j<n;j++,i++)
-      if(array[i] > array[j])
-	swap(array,i,j);
+    int i,j;
+    for(i=0;i<n;n--,i=0)
+	    for(j=i+1;j<n;j++,i++)
+	        if(array[i] > array[j])
+		        swap(array,i,j);
 }
 
 
 void swap(int *array,int i,int j)
 {
-  array[i] = array[j] + array[i] - (array[j] = array[i]);
+    array[i] = array[j] + array[i] - (array[j] = array[i]);
 }	
 
 int power(int p,int q)
 {
-  int i,j,sum=1;
-  for(i=0;i<q;i++)
-    sum*=p;
-  return sum;
+    int i,j,sum=1;
+    for(i=0;i<q;i++)
+	    sum*=p;
+    return sum;
 }
 
 
 void heapSort(int *array,int n)
 {
-  int i,j,temp=n;
+    int i,j,temp=n;
 
-  for( ;temp ; )
-  {
-    for(i=1;i<temp;i++)
-      if(array[0]<array[i])
-	swap(array,0,i);
-    swap(array,0,--temp);
-  }
+    for( ;temp ; )
+    {
+	    for(i=1;i<temp;i++)
+	        if(array[0]<array[i])
+		        swap(array,0,i);
+	    swap(array,0,--temp);
+    }
 }
 
 
 
 void quickSort(int *array,int n)
 {
-  int pivot=rand()%n,left=0,right=n-1;
-  printf("pivot = %d\n",pivot);		
-  printf("array[pivot] = %d\n",array[pivot]);	
+    int pivot=rand()%n,left=0,right=n-1;
+    printf("pivot = %d\n",pivot);		
+    printf("array[pivot] = %d\n",array[pivot]);	
 
-  while(pivot != left || pivot != right ) 
-  {
-    if( array[pivot] < array[right] ) 
-      right--;
-    else if(array[pivot] > array[right])
+    while(pivot != left || pivot != right ) 
     {
-      pivot = right;
-      swap(array,pivot,right);
-    }
+	    if( array[pivot] < array[right] ) 
+	        right--;
+	    else if(array[pivot] > array[right])
+	    {
+	        pivot = right;
+	        swap(array,pivot,right);
+	    }
 
-    if( array[pivot] < array[left] )
-      left++;		
-    else if ( array[pivot] > array[left] )
-    {
-      pivot = left;
-      swap(array,pivot,left);
+	    if( array[pivot] < array[left] )
+	        left++;		
+	    else if ( array[pivot] > array[left] )
+	    {
+	        pivot = left;
+	        swap(array,pivot,left);
+	    }
+	    console_print(array,n);
     }
-    console_print(array,n);
-  }
 }
 
 
